90d5e877e2afe4de786053990b93107cc8d13c5f
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2021-05-29 23:19:34 -0300

    add static html index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +25,51 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Space Traveling</title>
+      </Head>
+
+      <main>
+        <img src="/images/logo.svg" alt="logo"/>
+
+        <div>
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <time>15 Mar 2021</time>
+            <p>Joseph Oliveira</p>
+          </a>
+        </div>
+
+        <div>
+          <a href="#">
+            <strong>Criando um app CRA do zero</strong>
+            <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
+            <time>19 Abr 2021</time>
+            <p>Danilo Vieira</p>
+          </a>
+        </div>
+
+        <div>
+          <a href="#">
+            <strong>Como utilizar Hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <time>15 Mar 2021</time>
+            <p>Joseph Oliveira</p>
+          </a>
+        </div>
+
+        <span>Carregar mais posts</span>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
 //   // const postsResponse = await prismic.query(TODO);
 
-//   // TODO
 // };
