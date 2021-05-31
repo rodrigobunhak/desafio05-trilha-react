4665d2a84bde235f7e0dd94023d0579eb94b7913
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2021-05-31 20:50:23 -0300

    update page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
-import { GetStaticProps } from 'next';
-import Head from 'next/head';
-
 import { getPrismicClient } from '../services/prismic';
+import { GetStaticProps } from 'next';
+import { useState } from 'react';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import Prismic from '@prismicio/client';
+import Header from '../components/Header';
+import Link from 'next/link';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -25,89 +29,123 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
+
+  const formattedPost = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      )
+    }
+  })
+
+  const [posts, setPosts] = useState<Post[]>(formattedPost);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  async function handleNextPage(): Promise<void> {
+    if (currentPage !== 1 && nextPage === null) {
+      return;
+    }
+
+    const postsResults = await fetch(`${nextPage}`).then(response => 
+      response.json()  
+    );
+    setNextPage(postsResults.next_page)
+    setCurrentPage(postsResults.page)
+
+    const newPosts = postsResults.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy',
+          {
+            locale: ptBR,
+          }
+        ),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        }
+      }
+    })
+
+    setPosts([...posts, ...newPosts])
+  }
+
   return (
     <>
-      <Head>
-        <title>Space Traveling</title>
-      </Head>
-
-      <main className={styles.contentContainer}>
-        <img src="/images/logo.svg" alt="logo"/>
-
+      <main className={commonStyles.container}>
+      <Header/>
         <div className={styles.posts}>
-          <a href="#">
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div className={styles.info}>
-              <div className={styles.createdAt}>
-                <img src="/images/calendar.svg" alt="calendar_icon"/>
-                <time>15 Mar 2021</time>
-              </div>
-              <div className={styles.author}>
-                <img src="/images/author.svg" alt="author_icon"/>
-                <span>Joseph Oliveira</span>
-              </div>
-            </div>
-          </a>
+          { posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a className={styles.post}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <img src="/calendar.svg" alt="calendar_icon"/>
+                    <time>{post.first_publication_date}</time>
+                  </li>
+                  <li>
+                    <img src="/author.svg" alt="author_icon"/>
+                    <span>{post.data.author}</span>
+                  </li>
+                </ul>
+              </a>
+            </Link>
+          ))}
 
-          <a href="#">
-            <strong>Criando um app CRA do zero</strong>
-            <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
-            <div className={styles.info}>
-              <div className={styles.createdAt}>
-                <img src="/images/calendar.svg" alt="calendar_icon"/>
-                <time>19 Abr 2021</time>
-              </div>
-              <div className={styles.author}>
-                <img src="/images/author.svg" alt="author_icon"/>
-                <span>Danilo Vieira</span>
-              </div>
-            </div>
-          </a>
+          {nextPage && (
+            <button className={styles.loading} type="button" onClick={handleNextPage}>
+              Carregar mais posts
+            </button>
+          )}
 
-          <a href="#">
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de ciclos de vida.</p>
-            <div className={styles.info}>
-              <div className={styles.createdAt}>
-                <img src="/images/calendar.svg" alt="calendar_icon"/>
-                <time>15 Mar 2021</time>
-              </div>
-              <div className={styles.author}>
-                <img src="/images/author.svg" alt="author_icon"/>
-                <span>Joseph Oliveira</span>
-              </div>
-            </div>
-          </a>
-
-          <a href="#">
-            <strong>Criando um app CRA do zero</strong>
-            <p>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</p>
-            <div className={styles.info}>
-              <div className={styles.createdAt}>
-                <img src="/images/calendar.svg" alt="calendar_icon"/>
-                <time>19 Abr 2021</time>
-              </div>
-              <div className={styles.author}>
-                <img src="/images/author.svg" alt="author_icon"/>
-                <span>Danilo Vieira</span>
-              </div>
-            </div>
-          </a>
         </div>
-
-        <div className={styles.loading}>
-          <a>Carregar mais posts</a>
-        </div>
-        
       </main>
     </>
   )
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-// };
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      }
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts
+  }
+
+  return {
+    props: {
+      postsPagination
+    }
+  }
+};
