fe9db2cbedf1de118e489128d35aab30daa4d4c0
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2021-06-04 23:27:56 -0300

    testes ok

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,10 +1,19 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
 import Header from '../../components/Header';
+import Prismic from '@prismicio/client';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { useRouter } from 'next/router';
+import React from 'react';
+import Head  from 'next/head';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+// import { Head } from 'next/document';
+// import { Head } from 'next/document';
 
 interface Post {
   first_publication_date: string | null;
@@ -28,66 +37,129 @@ interface PostProps {
 }
 
 export default function Post({post}: PostProps ) {
+  
+  const totalWords = post.data.content.reduce((total, contentItem) => {
+    
+    total += contentItem.heading.split(' ').length;
+
+    const words = contentItem.body.map(item => item.text.split(' ').length); // 4
+
+    words.map(word => (total += word))
+
+    return total;
+  }, 0)
+
+  const readTime = Math.ceil(totalWords / 200)
+
+  const router = useRouter();
+
+  if(router.isFallback) {
+    return <h1>Carregando...</h1>
+  }
+
+  const formatedDate = format(
+    new Date(post.first_publication_date),
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  )
+
   return (
     <>
+      <Head>
+        <title>{post.data.title} | Space Traveling</title>
+      </Head>
       <Header />
-      <img src="/teste.png" alt="imagem" className={styles.banner} />
+      <img src={post.data.banner.url} alt="imagem" className={styles.banner} />
       <main className={commonStyles.container}>
-        <div className={styles.post}>
+        <div className={styles.header_post}>
           <h1>
-            Criando um app CRA do zero
+            {post.data.title}
           </h1>
           <ul>
             <li>
               <img src="/calendar.svg" alt="calendar_icon"/>
-              <time>15 Mar 2021</time>
+              <time>{formatedDate}</time>
             </li>
             <li>
               <img src="/author.svg" alt="author_icon"/>
-              <span>Joseph Oliveira</span>
+              <span>{post.data.author}</span>
             </li>
             <li>
-              <img src="/clock.svg" alt="author_icon"/>
-              <span>4 min</span>
+              <img src="/clock.svg" alt="clock_icon"/>
+              <span>{`${readTime} min`}</span>
             </li>
           </ul>
-          <div className={styles.content}>
-            <h2>Proin et varius</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/><br/>
-
-Nullam dolor sapien, vulputate eu diam at, condimentum hendrerit tellus. Nam facilisis sodales felis, pharetra pharetra lectus auctor sed.
-
-Ut venenatis mauris vel libero pretium, et pretium ligula faucibus.<br/><br/> Morbi nibh felis, elementum a posuere et, vulputate et erat. Nam venenatis.
-            </p>
-            <h2>Proin et varius</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/><br/>
-
-Nullam dolor sapien, <strong>vulputate eu diam at</strong>, condimentum hendrerit tellus. Nam facilisis sodales felis, pharetra pharetra lectus auctor sed.
-
-Ut venenatis mauris vel libero pretium, et pretium ligula faucibus.<br/><br/> Morbi nibh felis, elementum a posuere et, vulputate et erat. Nam venenatis.
-            </p>
-            <h2>Cras laoreet mi</h2>
-            <p>Nulla auctor sit amet quam vitae commodo. Sed risus justo, vulputate quis neque eget, dictum sodales sem. In eget felis finibus, mattis magna a, efficitur ex. <br/><br/>Curabitur vitae justo consequat sapien gravida auctor a non risus. Sed malesuada mauris nec orci congue, interdum efficitur urna dignissim. Vivamus cursus elit sem, vel facilisis nulla pretium consectetur. Nunc congue.<br/><br/>
-
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <br/><br/>Aliquam consectetur massa nec metus condimentum, sed tincidunt enim tincidunt. Vestibulum fringilla risus sit amet massa suscipit eleifend. Duis eget metus cursus, suscipit ante ac, iaculis est. Donec accumsan enim sit amet lorem placerat, eu dapibus ex porta. Etiam a est in leo pulvinar auctor. Praesent sed vestibulum elit, consectetur egestas libero.
-            </p>
-          </div>
         </div>
+
+          {post.data.content.map(content => {
+            return (
+              <div className={styles.content_post} key={content.heading}>
+                <h2>{content.heading}</h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                  __html: RichText.asHtml(content.body)
+                }}/>
+              </div>
+            )
+          })}
+
       </main>
     </>
   )
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts'),
+  ]);
 
-//   // TODO
-// };
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        slug: post.uid,
+      }
+    }
+  })
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  return {
+    paths,
+    fallback: true,
+  }
+};
 
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async context => {
+  const prismic = getPrismicClient();
+  const { slug } = context.params;
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
+      banner: {
+        url: response.data.banner.url,
+      },
+      content: response.data.content.map(content => {
+        return {
+          heading: content.heading,
+          body: [...content.body]
+        }
+      })
+    }
+  }
+
+  // console.log(JSON.stringify(post.data.content, null, 2))
+
+  return {
+    props: {
+      post,
+    }
+  }
+};
+
